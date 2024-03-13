cef70dad37c44c77b392fc7256cdaa0d5e69f4b0
Soshtakovich <125446692+Soshtakovich@users.noreply.github.com>
2024-03-13 13:14:38 +0200

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 --SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id=states.id
 ORDER BY cities.id ASC;
