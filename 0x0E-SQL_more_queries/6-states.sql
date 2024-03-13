75ce18a45a6a870e81e1a0bc5c69974791e666e7
Soshtakovich <125446692+Soshtakovich@users.noreply.github.com>
2024-03-13 13:01:36 +0200

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,2 +1,7 @@
---creates the database hbtn_0d_usa and the table states
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
